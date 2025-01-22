ba09cde74d0b9efd57adff7441e5b17f5c3c6a2d
Vitsman <jigulsky.vitalik@yandex.ru>
2025-01-22 11:40:41 +0300

    another_test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -53,7 +53,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
 DATABASES = {
     'default': {
         # Меняем настройку Django: теперь для работы будет использоваться
